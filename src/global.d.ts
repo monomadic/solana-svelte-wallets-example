6f7db86207d43f5d6acf38b95fc165c8a94653a0
Monomadic <deathdisco@protonmail.com>
2021-10-15 13:25:14 +0800

    wallet selection working

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="@sveltejs/kit" />
+import { Buffer } from 'buffer';
+import process from 'process';
+
+
+globalThis.Buffer = Buffer;
+globalThis.process = process;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { useWallet, initWallet } from '$lib/wallet/useWallet';
+	import type { WalletName } from '@solana/wallet-adapter-wallets';
+	import { Buffer } from 'buffer';
+
+	const localStorageKey = 'walletAdapter';
+
+	onMount(async () => {
+		// setup some globals
+		global.Buffer = Buffer;
+
+		import('process').then((process) => {
+			global.process = process;
+		});
+
+		const wallets = await import('@solana/wallet-adapter-wallets');
+
+		await initWallet({
+			wallets: [wallets.getPhantomWallet(), wallets.getLedgerWallet()],
+			autoConnect: true,
+			localStorageKey
+		});
+	});
+
+	async function selectWallet(walletName: string) {
+		try {
+			$useWallet.select(walletName as WalletName);
+			$useWallet.connect();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+</script>
+
+<div class="wrapper-content">
+	{#if $useWallet?.connected}
+		<button on:click={() => $useWallet.disconnect()}>disconnect wallet</button>
+	{:else}
+		<button on:click={() => selectWallet('Phantom')}>Phantom</button>
+		<button on:click={() => selectWallet('Ledger')}>Ledger</button>
+	{/if}
+</div>
+
+<svelte:head>
+	<script>
+		window.global = window;
+		global = globalThis; // for solana web3 repo
+	</script>
+</svelte:head>
